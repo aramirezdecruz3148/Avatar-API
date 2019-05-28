ae0bce71cacf87d672c4929699831cbd89ddcd04
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-28 16:34:49 -0700

    finished styling my page out with the static list stand-in

--- src/components/AvatarList.js
+++ src/components/AvatarList.js
@@ -4,16 +4,16 @@ class AvatarList extends Component {
 
     renderTemplate() {
         return /*html*/`
-            <ul class="queen-list">
-                <li>
+            <ul class="avatar-list">
+                <li class="avatar-item">
                     <h3>46th Earth King</h3>
                     <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441" alt="46th Earth King">
                 </li>
-                <li>
+                <li class="avatar-item">
                     <h3>Aang</h3>
                     <img src="https://vignette.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png/revision/latest?cb=20130612174003" alt="Aang">
                 </li>
-                <li>
+                <li class="avatar-item">
                     <h3>Aang (games)</h3>
                     <img src="https://vignette.wikia.nocookie.net/avatar/images/2/21/Into_The_Inferno_Aang.png/revision/latest?cb=20131009060746" alt="Aang (games)">
                 </li>
