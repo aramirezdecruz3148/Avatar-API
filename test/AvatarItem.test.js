7f92d83a90b525cfb32bac3d8efdf5c3c624f28d
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-28 16:41:48 -0700

    created my DDD test for my AvatarItem, set it all up for a failing test

---
+++ test/AvatarItem.test.js
@@ -0,0 +1,21 @@
+import AvatarItem from '../src/components/AvatarItem.js';
+const test = QUnit.test;
+
+QUnit.module('Avatar Item');
+
+test('renders template from data', assert => {
+    //arrange
+    const character = {
+        _id: '5cdf0769b6e02a467e3e766b',
+        photoUrl: 'https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441',
+        name: '46th Earth King'
+    };
+    
+    //act
+    const avatarItem = new AvatarItem({ character });
+    const rendered = avatarItem.renderTemplate();
+
+    //assert
+    assert.htmlEqual(rendered, /*html*/`
+        `);
+});
--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,2 @@
 import './html-equal.js';
+import './AvatarItem.test.js';
