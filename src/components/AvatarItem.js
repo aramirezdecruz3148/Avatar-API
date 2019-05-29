7524c83ce530c992d29fbe663ff8d3f2f9925043
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-28 17:36:41 -0700

    added logic to support a placeholder image if data has none within my AvatarItem component

--- src/components/AvatarItem.js
+++ src/components/AvatarItem.js
@@ -4,6 +4,9 @@ class AvatarItem extends Component {
 
     renderTemplate() {
         const character = this.props.character;
+        if(!character.photoUrl) {
+            character.photoUrl = '../../../assets/placeholder.jpg';
+        }
         return /*html*/`
             <li class="avatar-item">
                 <h3>${character.name}</h3>
